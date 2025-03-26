1186e94b945c9b905dab0f786f510eabb4743643
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-03-26 20:16:26 -0300

    Completed channel CRUD

--- back-end/src/main/java/com/example/demo/features/channel/ChannelController.java
+++ back-end/src/main/java/com/example/demo/features/channel/ChannelController.java
@@ -1,9 +1,15 @@
 package com.example.demo.features.channel;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +29,27 @@ public class ChannelController {
                                                         ChannelDTO 
                                                         channelDTO) {
         return channelService.create(channelDTO);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<HashMap<String, String>> put(
+        @PathVariable UUID id, @RequestBody ChannelDTO channelDTO) {
+
+        return channelService.update(id, channelDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChannelDTO>> getAll() {
+        return channelService.getAll();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ChannelDTO> getById(@PathVariable UUID id) {
+        return channelService.getById(id);
+    }
+    
+    @DeleteMapping("{id}")
+    public ResponseEntity<ChannelDTO> deleteById(@PathVariable UUID id) {
+        return channelService.deleteById(id);
     }
 }
