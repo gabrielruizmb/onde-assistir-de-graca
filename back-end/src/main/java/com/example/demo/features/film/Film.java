270efa12da74dec8ef0d722702f4d332f75a7d45
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-03-26 21:43:13 -0300

    Added film feature

--- back-end/src/main/java/com/example/demo/features/film/Film.java
+++ back-end/src/main/java/com/example/demo/features/film/Film.java
@@ -1,23 +1,45 @@
-// package com.example.demo.features.film;
+package com.example.demo.features.film;
 
-// import java.util.List;
-// import java.util.UUID;
+import java.nio.channels.Channel;
+import java.util.List;
+import java.util.UUID;
+import java.util.Locale.Category;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import lombok.Getter;
-// import lombok.Setter;
+import com.example.demo.features.genre.Genre;
 
-// @Getter
-// @Setter
-// @Entity
-// public class Film {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.UUID)
-//     private UUID id;
-//     private String title;
-//     private int year;
-//     private String description;
-// }
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String title;
+
+    private int year;
+
+    private String description;
+
+    @OneToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category category;
+
+    @OneToMany(mappedBy = "film")
+    private List<Genre> genres;
+
+    @OneToMany(mappedBy = "film")
+    private List<Channel> channels;
+}
