0632c05aae8c4f81c762fa0ffeb695823206f294
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-03-29 23:09:15 -0300

    :sparkle: Completed Film CRUD

--- back-end/src/main/java/com/example/demo/features/film/FilmController.java
+++ back-end/src/main/java/com/example/demo/features/film/FilmController.java
@@ -1,9 +1,15 @@
 package com.example.demo.features.film;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +28,27 @@ public class FilmController {
     public ResponseEntity<HashMap<String, String>> post(@RequestBody 
                                                         FilmDTO filmDTO) {
         return filmService.create(filmDTO);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<HashMap<String, String>> put(
+        @PathVariable UUID id, @RequestBody FilmDTO filmDTO
+    ) {
+        return filmService.update(id, filmDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FilmDTO>> getAll() {
+        return filmService.getAll();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<FilmDTO> getById(@PathVariable UUID id) {
+        return filmService.getById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<FilmDTO> deleteById(@PathVariable UUID id) {
+        return filmService.deleteById(id);
     }
 }
--- back-end/src/main/java/com/example/demo/features/film/FilmService.java
+++ back-end/src/main/java/com/example/demo/features/film/FilmService.java
@@ -1,6 +1,8 @@
 package com.example.demo.features.film;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
@@ -42,11 +44,43 @@ public class FilmService {
         }
     }
 
-    public ResponseEntity<HashMap<String, String>> update(UUID id, FilmDTO filmDTO) {
+    public ResponseEntity<HashMap<String, String>> update(UUID id, 
+                                                          FilmDTO filmDTO) {
 
         if (!filmRepository.existsById(id)) 
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 
         return create(filmDTO);
+    }
+
+    public ResponseEntity<List<FilmDTO>> getAll() {
+        List<FilmDTO> filmDTOs = new ArrayList<>();
+        List<Film> films = filmRepository.findAll();
+
+        for (Film film : films) {
+            filmDTOs.add(film.convertToDTO());
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(filmDTOs);
+    }
+
+    public ResponseEntity<FilmDTO> getById(UUID id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(
+                filmRepository.findById(id).get().convertToDTO()
+            );
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    public ResponseEntity<FilmDTO> deleteById(UUID id) {
+
+        if(!filmRepository.existsById(id))
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+        filmRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+
     }
 }
