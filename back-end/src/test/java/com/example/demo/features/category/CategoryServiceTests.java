cbc1ad14ca94c7d3ed6a6af9c17891503b584de1
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-05-06 21:43:25 -0300

    Added scenario002 in CategoryServiceTests.java

--- back-end/src/test/java/com/example/demo/features/category/CategoryServiceTests.java
+++ back-end/src/test/java/com/example/demo/features/category/CategoryServiceTests.java
@@ -5,6 +5,8 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,7 +26,7 @@ public class CategoryServiceTests {
     CategoryRepository categoryRepository;
 
     @Test
-    @DisplayName("Cenário 001 - Testando método getAll")
+    @DisplayName("Cenário 001 - Teste unitário - getAll")
     void scenario001() {
 
         List<Category> categories = new ArrayList<>();
@@ -34,5 +36,29 @@ public class CategoryServiceTests {
         ResponseEntity<List<CategoryDTO>> list = categoryService.getAll();
 
         assertEquals(HttpStatus.OK, list.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Cenário 002 - Teste unitário - getById com id válido")
+    void scenario002() {
+         
+        UUID id = new UUID(0, 0);
+        Category category = new Category(null, null, null);
+        Optional<Category> optionalCategory = Optional.of(category);
+
+        when(categoryRepository.findById(id)).thenReturn(optionalCategory);
+
+        categoryService.getById(id);
+    }
+
+    @Test
+    @DisplayName("Cenário 003 - Teste unitário - getById com id inválido")
+    void scenario003() {
+        Category category = new Category(null, null, null);
+        Optional<Category> optionalCategory = Optional.of(category);
+
+        when(categoryRepository.findById(null)).thenReturn(optionalCategory);
+
+        categoryService.getById(null);
     }
 }
