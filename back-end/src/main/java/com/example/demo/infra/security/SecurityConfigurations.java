fc0859652ba0dd86090de082bd6db30be91cba83
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-05-10 00:28:28 -0300

    Added SecurityConfigurations.java

---
+++ back-end/src/main/java/com/example/demo/infra/security/SecurityConfigurations.java
@@ -0,0 +1,69 @@
+package com.example.demo.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfigurations {
+    
+    final SecurityFilter securityFilter;
+
+    public SecurityConfigurations(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+        HttpSecurity httpSecurity
+    ) throws Exception {
+
+        return httpSecurity
+                .csrf((csrf) -> csrf.disable())
+                .cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers(HttpMethod.GET, "/api/categories")
+                        .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/channels")
+                        .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/films")
+                        .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/films/{id}")
+                        .permitAll()
+                    .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                    securityFilter, 
+                    UsernamePasswordAuthenticationFilter.class
+                )
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
