45e04064435447e4a9c3780dfbf29eb84c60c5ae
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-05-10 00:33:07 -0300

    Added Security configurations

--- back-end/src/main/java/com/example/demo/infra/security/SecurityFilter.java
+++ back-end/src/main/java/com/example/demo/infra/security/SecurityFilter.java
@@ -45,8 +45,9 @@ public class SecurityFilter extends OncePerRequestFilter {
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
-            filterChain.doFilter(request, response);
         }
+        
+        filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request) {
