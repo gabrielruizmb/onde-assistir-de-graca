6a983cf782320473cfc76233bfcaac25ee9048ad
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-05-09 23:52:46 -0300

    Added SecurityFilter.java

--- back-end/src/main/java/com/example/demo/infra/security/TokenService.java
+++ back-end/src/main/java/com/example/demo/infra/security/TokenService.java
@@ -6,7 +6,10 @@ import java.time.ZoneOffset;
 
 import org.springframework.stereotype.Service;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.demo.features.user.User;
 
 @Service
@@ -15,12 +18,39 @@ public class TokenService {
     String secret = "T3RC3SR3PUSEHTS1S013ST3RC3SR3PUSEHTS1S013ST3RC3SR3PUSEHT";
 
     public String generateToken(User user) {
-        Algorithm algorithm = Algorithm.HMAC256(secret);
 
-        return "";
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                              .withIssuer("onde_assistir_de_graca")
+                              .withSubject(user.getEmail())
+                              .withExpiresAt(generateExpirationTime())
+                              .sign(algorithm);
+
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar o token: ", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                      .withIssuer("onde_assistir_de_graca")
+                      .build()
+                      .verify(token)
+                      .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
     }
 
     private Instant generateExpirationTime() {
+        
         return LocalDateTime.now().plusHours(1)
                             .toInstant(ZoneOffset.of("-03:00"));
     }
