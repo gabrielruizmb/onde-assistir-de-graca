0056b8e01b47ca144f36e42b2889e872fa136d3e
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-05-19 21:17:04 -0300

    Added guards /routes.guard.ts

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/layout/login/login.component';
 import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
-import { routesGuard } from './services/routes.guard';
+import { routesGuard } from './guards/routes.guard';
 
 export const routes: Routes = [
     {path: "login", component: LoginComponent},
