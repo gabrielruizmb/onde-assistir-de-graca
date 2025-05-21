33b06e03997251d4d971b1dea55d302c2f6b895b
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-05-20 21:50:07 -0300

    Added add-film-button

---
+++ front-end/src/app/components/films/film-form/film-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-form',
+  imports: [],
+  templateUrl: './film-form.component.html',
+  styleUrl: './film-form.component.css'
+})
+export class FilmFormComponent {
+
+}
