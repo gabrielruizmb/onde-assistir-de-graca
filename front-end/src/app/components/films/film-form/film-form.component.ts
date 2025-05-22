e612e13b231c49e348d5a771576caa5d73619ce4
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-05-22 14:27:12 -0300

    Added film-form

--- front-end/src/app/components/films/film-form/film-form.component.ts
+++ front-end/src/app/components/films/film-form/film-form.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Film } from '../../../models/film';
+import { HeaderComponent } from '../../layout/header/header.component';
+import { CategoryService } from '../../../services/category.service';
+import { Category } from '../../../models/category';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-film-form',
-  imports: [],
+  imports: [HeaderComponent, FormsModule],
   templateUrl: './film-form.component.html',
   styleUrl: './film-form.component.css'
 })
 export class FilmFormComponent {
 
+  film: Film = new Film();
+  categoryService = inject(CategoryService);
+  categoriesList: Category[] = [];
+
+  constructor() {
+    this.getAllCategories();
+  }
+
+  getAllCategories() {
+    this.categoryService.getAll().subscribe({
+      next: (returnedCategories) => {
+        this.categoriesList = returnedCategories;
+      },
+      error: (error) => {
+        console.log("Falha ao buscar a lista de categorias.");
+      }
+    })
+  }
 }
